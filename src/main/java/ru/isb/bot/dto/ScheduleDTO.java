8ef8252dc61ab1d0d51cbf5f2df5dafa9653691a
kuzaevrr <kuzaevrr>
2022-09-29 12:13:21 +0500

    update parse Json

--- src/main/java/ru/isb/bot/dto/ScheduleDTO.java
+++ src/main/java/ru/isb/bot/dto/ScheduleDTO.java
@@ -15,8 +15,6 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-@JsonDeserialize
-@JsonSerialize
 public class ScheduleDTO implements Serializable {
 
     private String building_id;
