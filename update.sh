256ddde0c34b0c45e4ff61257fa0be12f40678cd
kuzaevrr <kuzaevrr@proitr.ru>
2023-04-21 12:49:01 +0500

    скрипт апдейта

--- update.sh
+++ update.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 systemctl stop isb-bot
+send "systemctl stop isb-bot -> success"
 git pull
+send "git pull -> success"
 mvn clean install -Dmaven.test.skip
+send "mvn clean install -Dmaven.test.skip -> success"
 systemctl start isb-bot
+send "systemctl start isb-bot -> success"
