a4529e4e96ef23e4120310d82466dfdfe83da076
kuzaevrr <kuzaevrr@proitr.ru>
2023-04-21 12:51:15 +0500

    скрипт апдейта

--- update.sh
+++ update.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-systemctl stop isb-bot
-send "systemctl stop isb-bot -> success"
-git pull
-send "git pull -> success"
-mvn clean install -Dmaven.test.skip
-send "mvn clean install -Dmaven.test.skip -> success"
-systemctl start isb-bot
-send "systemctl start isb-bot -> success"
+echo "Start update isb-bot";
+systemctl stop isb-bot;
+echo "systemctl stop isb-bot -> success";
+git pull;
+echo "git pull -> success";
+mvn clean install -Dmaven.test.skip;
+echo "mvn clean install -Dmaven.test.skip -> success";
+systemctl start isb-bot;
+echo "systemctl start isb-bot -> success";
