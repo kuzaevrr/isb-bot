4ef97aa24a2bd6184ec4e9cea37ff021b27a6218
kuzaev <kuzaev@proitr.ru>
2023-09-08 17:07:13 +0500

    Добавлена запись файла с копией

--- src/main/java/ru/isb/bot/utils/StringUtils.java
+++ src/main/java/ru/isb/bot/utils/StringUtils.java
@@ -5,7 +5,7 @@ public class StringUtils {
     private static final String COPY_POSTFIX = " (копия)";
 
     public static String copyFileName(String fileName) {
-        String [] prefixAndPostfix = fileName.split(".");
+        String [] prefixAndPostfix = fileName.split("\\.");
         return prefixAndPostfix.length == 2 ? prefixAndPostfix[0] + COPY_POSTFIX + "." + prefixAndPostfix[1] : fileName + COPY_POSTFIX;
     }
 
